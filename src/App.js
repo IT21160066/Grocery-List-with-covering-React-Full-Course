a6ed8c58dedd09d084bb9f51f8e1f36da14fb5bf
Pasindu Perakum <it21160066my.sliit.lk>
2023-12-07 22:22:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import Header from './Header';
-import SearchItem from './SearchItem';
-import AddItem from './AddItem';
-import Content from './Content';
-import Footer from './Footer';
-import { useState, useEffect } from 'react';
+import Header from "./Header";
+import SearchItem from "./SearchItem";
+import AddItem from "./AddItem";
+import Content from "./Content";
+import Footer from "./Footer";
+import { useState, useEffect } from "react";
+import apiRequest from "./apiRequest";
 
 function App() {
-
-  const API_URL = 'http://localhost:3500/items';
+  const API_URL = "http://localhost:3500/items";
 
   const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('');
-  const [search, setSearch] = useState('');
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
   const [fetchError, setFetchError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchItems = async () =>{
-      try{
+    const fetchItems = async () => {
+      try {
         const response = await fetch(API_URL);
-        if(!response.ok) throw Error('Did not recive expected data');
+        if (!response.ok) throw Error("Did not recive expected data");
         const listItems = await response.json();
         //console.log(listItems)
         setItems(listItems);
@@ -29,63 +29,92 @@ function App() {
       } finally {
         setIsLoading(false);
       }
-    }
+    };
     setTimeout(() => {
       fetchItems();
-    }, 2000)
+    }, 2000);
   }, []);
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id, checked: false, item};
+    const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
-  }
 
-  const hanndleCheck = (id) => {
-      //console.log(`key ${id}`)
-      const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
-      setItems(listItems);
-    }
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(myNewItem),
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
+  };
 
-  const handleDelete = (id) => {
-      //console.log(id)
-      const listItems = items.filter((item) => item.id !== id)
-      setItems(listItems);
-  }
+  const hanndleCheck = async (id) => {
+    //console.log(`key ${id}`)
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) setFetchError(result);
+  };
+
+  const handleDelete = async (id) => {
+    //console.log(id)
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+
+    const deleteOptions = { method: "DELETE" };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) setFetchError(result);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!newItem) return;
+    if (!newItem) return;
     console.log(newItem);
     addItem(newItem);
-    setNewItem('');
-    console.log('submitted');
-  }
+    setNewItem("");
+    console.log("submitted");
+  };
 
   return (
     <div className="App">
-
-      <Header title='Groceries List'/>
+      <Header title="Groceries List" />
       <AddItem
         newItem={newItem}
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
-      <SearchItem
-        search={search}
-        setSearch={setSearch}
-      />
+      <SearchItem search={search} setSearch={setSearch} />
       <main>
         {isLoading && <p>Loading Items...</p>}
-        {fetchError && <p style={{ color: 'red'}}>{`Error: ${fetchError}`}</p>}
-        {!fetchError && !isLoading && <Content 
-          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-          hanndleCheck={hanndleCheck}
-          handleDelete={handleDelete}
-        />}
+        {fetchError && <p style={{ color: "red" }}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && !isLoading && (
+          <Content
+            items={items.filter((item) =>
+              item.item.toLowerCase().includes(search.toLowerCase())
+            )}
+            hanndleCheck={hanndleCheck}
+            handleDelete={handleDelete}
+          />
+        )}
       </main>
-      <Footer length = {items.length}/>
+      <Footer length={items.length} />
     </div>
   );
 }
